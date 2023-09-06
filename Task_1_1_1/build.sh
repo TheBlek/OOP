8934d4fd1e01f48b53346a45a313e2ec3d80d8b8
Kuklin Georgiy <theblek@yandex-team.ru>
2023-09-06 20:53:21 +0700

    Rename package

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-javadoc -d docs -sourcepath ./app/src/main/java task_1_1_1
+javadoc -d docs -sourcepath ./app/src/main/java heapsort
 mkdir -p build
-javac ./app/src/main/java/task_1_1_1/App.java -d build
+javac ./app/src/main/java/heapsort/App.java -d build
 cd build
-java task_1_1_1.App
+java heapsort.App
 
