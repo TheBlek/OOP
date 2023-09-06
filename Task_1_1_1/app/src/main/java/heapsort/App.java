4b241e4723bbb6d77032413e96e705243379f5ae
Kuklin Georgiy <theblek@yandex-team.ru>
2023-09-06 21:19:54 +0700

    Remove main

--- Task_1_1_1/app/src/main/java/heapsort/App.java
+++ Task_1_1_1/app/src/main/java/heapsort/App.java
@@ -43,11 +43,4 @@ public class App {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        int[] res = App.heapsort(new int[]{5, 4, 3, 2, 1});
-        for (int value : res) {
-            System.out.println(value);
-        }
-    }
 }
