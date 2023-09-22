7d5ff1d48897280b201a1fe84f3fc1f736cb862c
Kuklin Georgiy <theblek@yandex-team.ru>
2023-09-22 09:35:01 +0700

    Port tests to assertArrayEquals

--- Task_1_1_1/app/src/test/java/heapsort/AppTest.java
+++ Task_1_1_1/app/src/test/java/heapsort/AppTest.java
@@ -1,7 +1,7 @@
 
 package ru.nsu.kuklin.heapsort;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -17,55 +17,55 @@ public class AppTest {
     @Test
     @DisplayName("empty")
     public void heapsortTestEmpty() {
-        int[] res = new int[]{};
-        App.heapsort(res);
-        assertTrue(Arrays.equals(res, new int[]{}));
+        int[] actual = new int[]{};
+        App.heapsort(actual);
+        assertArrayEquals(actual, new int[]{});
     }
 
     @Test
     @DisplayName("one element")
     public void heapsortTestOne() {
-        int[] res = new int[]{-100};
-        App.heapsort(res);
-        assertTrue(Arrays.equals(res, new int[]{-100}));
+        int[] actual = new int[]{-100};
+        App.heapsort(actual);
+        assertArrayEquals(actual, new int[]{-100});
     }
 
     @Test
     @DisplayName("sorted")
     public void heapsortTestSorted() {
-        int[] res = new int[]{1, 2, 3, 4, 5, 6};
-        App.heapsort(res);
-        assertTrue(Arrays.equals(res, new int[]{1, 2, 3, 4, 5, 6}));
+        int[] actual = new int[]{1, 2, 3, 4, 5, 6};
+        App.heapsort(actual);
+        assertArrayEquals(actual, new int[]{1, 2, 3, 4, 5, 6});
     }
 
     @Test
     @DisplayName("reverse-sorted")
     public void heapsortTestReverseSorted() {
-        int[] res = new int[]{6, 5, 4, 3, 2, 1};
-        App.heapsort(res);
-        assertTrue(Arrays.equals(res, new int[]{1, 2, 3, 4, 5, 6}));
+        int[] actual = new int[]{6, 5, 4, 3, 2, 1};
+        App.heapsort(actual);
+        assertArrayEquals(actual, new int[]{1, 2, 3, 4, 5, 6});
     }
 
     @Test
     @DisplayName("sorted-shift")
     public void heapsortTestSortedShift() {
-        int[] res = new int[]{6, -1, 2, 3, 4, 5};
-        App.heapsort(res);
-        assertTrue(Arrays.equals(res, new int[]{-1, 2, 3, 4, 5, 6}));
+        int[] actual = new int[]{6, -1, 2, 3, 4, 5};
+        App.heapsort(actual);
+        assertArrayEquals(actual, new int[]{-1, 2, 3, 4, 5, 6});
     }
 
     @Test
     @DisplayName("big-ass")
     public void heapsortTestBig() {
         int size = 1000000;
-        int[] input = new int[size];
+        int[] expected = new int[size];
         Random rng = new Random();
         for (int i = 0; i < size; i++) {
-            input[i] = rng.nextInt() % 10000;
+            expected[i] = rng.nextInt() % 10000;
         }
-        int[] mine = input.clone();
-        App.heapsort(mine);
-        Arrays.sort(input);
-        assertTrue(Arrays.equals(mine, input));
+        int[] actual = expected.clone();
+        App.heapsort(actual);
+        Arrays.sort(expected);
+        assertArrayEquals(actual, expected);
     }
 }
