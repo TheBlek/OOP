7c31ffa4aa60ab3e4f7e2387f2ce63350cbe5a7c
TheBlek <TheBlek@users.noreply.github.com>
2023-09-26 05:15:36 +0000

    Deploying to gh-pages from @ TheBlek/OOP@78ef309eb9c9a466e954f373134e70c1e140f3ca 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"polynomial"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kuklin.polynomial"}];updateSearchResults();
