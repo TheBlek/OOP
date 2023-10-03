fa5ffedf08c7291f644bb182f6b2870610f257c2
TheBlek <TheBlek@users.noreply.github.com>
2023-10-03 15:07:12 +0000

    Deploying to gh-pages from @ TheBlek/OOP@78910a51b2686b7c6d1e2b19e05d22648ef966eb 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kuklin.polynomial","l":"App"},{"p":"ru.nsu.kuklin.polynomial","l":"Polynomial"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kuklin.polynomial","l":"Polynomial"}];updateSearchResults();
