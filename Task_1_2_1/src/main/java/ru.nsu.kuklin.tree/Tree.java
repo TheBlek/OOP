06df5a1446b0227de6e8a352b702f1ec4607acf9
Kuklin Georgiy <theblek@yandex-team.ru>
2023-10-17 08:47:40 +0700

    Hash code is linear :(

--- Task_1_2_1/src/main/java/ru.nsu.kuklin.tree/Tree.java
+++ Task_1_2_1/src/main/java/ru.nsu.kuklin.tree/Tree.java
@@ -132,8 +132,7 @@ public class Tree<T> implements Iterable<T> {
      * 2. 0bytes and awareness of parents vs 8bytes (pbbly) for parent link
      * 3. Same tree in multiple hierarchies without additional nonsense 
      * or
-     * 1. O(1) vs O(tree size) state change check
-     * 2. O(1) vs O(tree size) state snapshot memory footprint
+     * 1. O(1) vs O(tree size) state snapshot memory footprint
      *
      * @return iterator over a collection
      */
