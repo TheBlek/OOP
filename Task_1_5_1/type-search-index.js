e8234b2039268828419651db868cd12f74f7c4d4
TheBlek <TheBlek@users.noreply.github.com>
2023-12-12 13:29:37 +0000

    Deploying to gh-pages from @ TheBlek/OOP@ef1ddd87ab763c76a785080079347a3e8984df3c 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kuklin.calculator","l":"App"},{"p":"ru.nsu.kuklin.calculator","l":"Calculator"},{"p":"ru.nsu.kuklin.calculator","l":"Complex"}];updateSearchResults();
