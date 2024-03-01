ba2961d66d97e634cba073fa25a4a7d76447c4d0
TheBlek <TheBlek@users.noreply.github.com>
2024-03-01 06:39:52 +0000

    Deploying to gh-pages from @ TheBlek/OOP@4c777fbfa79a6b8430a95487ad18f6448e69434b 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kuklin.parallelprime","l":"CompositeNumberDetector"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kuklin.parallelprime","l":"CompositeNumberDetector"},{"p":"ru.nsu.kuklin.parallelprime","l":"ParallelStreamDetector"},{"p":"ru.nsu.kuklin.parallelprime","l":"RawThreadsDetector"},{"p":"ru.nsu.kuklin.parallelprime","l":"SequentialDetector"}];updateSearchResults();
