6e80e5bdff644d4f369613d2a86fa4c8a7e648fb
Georgiy Kuklin <egorka99966@gmail.com>
2024-03-14 08:46:15 +0700

    Test factory .map method

--- Task_2_2_1/src/test/java/ru/nsu/kuklin/pizzeria/FactoryTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/kuklin/pizzeria/FactoryTest.java
@@ -25,6 +25,23 @@ public class FactoryTest {
         assertEquals(state.getStorage(), res.getStorage());
     }
     @Test
+    public void testBakerFactoryMap() {
+        var state = new State(100);
+        var data = new BakerData[]{
+                new BakerData("John", 82),
+                new BakerData("Mike", -12),
+                new BakerData("Авдотьевна", 0),
+        };
+        var factory = new DefaultBakerFactory(state);
+        var res = factory.map(data);
+        for (int i = 0; i < res.size(); i++) {
+            assertEquals(data[i].timePerPizza(), res.get(i).getTimeToPizza());
+            assertEquals(state.getOrders(), res.get(i).getOrders());
+            assertEquals(state.getStorage(), res.get(i).getStorage());
+        }
+    }
+
+    @Test
     public void testCustomerFactory() {
         var state = new State(100);
         var data = new CustomerData("John", new Order[] {new Order(0, "PIZZA"), new Order(-123, "Another one")});
@@ -34,12 +51,42 @@ public class FactoryTest {
         assertEquals(state.getOrders(), res.getOrders());
     }
     @Test
+    public void testCustomerFactoryMap() {
+        var state = new State(100);
+        var data = new CustomerData[]{
+                new CustomerData("John", new Order[] {new Order(0, "PIZZA"), new Order(-123, "Another one")}),
+                new CustomerData("Mike", new Order[] {new Order(1, "PIZZA1"), new Order(-122, "Another oneasdfasd")}),
+                new CustomerData("Авдотьевна", new Order[] {new Order(2, "PIZZA2"), new Order(-0, "Another onesdf")}),
+        };
+        var factory = new DefaultCustomerFactory(state);
+        var res = factory.map(data);
+        for (int i = 0; i < res.size(); i++) {
+            assertArrayEquals(data[i].orders(), res.get(i).getToPlace());
+            assertEquals(state.getOrders(), res.get(i).getOrders());
+        }
+    }
+    @Test
     public void testDelivererFactory() {
         var state = new State(100);
-        DelivererData data = new DelivererData("John", 82);
+        var data = new DelivererData("John", 82);
         var factory = new DefaultDelivererFactory(state);
         var res = factory.construct(data);
         assertEquals(82, res.getDeliveryTime());
         assertEquals(state.getStorage(), res.getStorage());
+    }
+    @Test
+    public void testDelivererFactoryMap() {
+        var state = new State(100);
+        var data = new DelivererData[]{
+                new DelivererData("John", 90),
+                new DelivererData("Mike", -42),
+                new DelivererData("Авдотьевна", 0),
+        };
+        var factory = new DefaultDelivererFactory(state);
+        var res = factory.map(data);
+        for (int i = 0; i < res.size(); i++) {
+            assertEquals(data[i].deliveryTime(), res.get(i).getDeliveryTime());
+            assertEquals(state.getStorage(), res.get(i).getStorage());
+        }
     }
 }
