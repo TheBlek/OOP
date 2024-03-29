520d69d19548871c8e3cb6a106be88dcdd0c5c11
Georgiy Kuklin <egorka99966@gmail.com>
2024-03-29 22:20:53 +0700

    Add stub test

---
+++ Task_2_3_1/src/test/java/ru/nsu/kuklin/snake/SnakeTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.kuklin.snake;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SnakeTest {
+    @Test
+    void stubTest() {
+        assertTrue(true);
+    }
+}
