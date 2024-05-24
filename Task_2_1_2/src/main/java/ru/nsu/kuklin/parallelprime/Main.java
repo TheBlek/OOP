87472bffef36a722bafc31579bfabcc793ec1942
Georgiy Kuklin <egorka99966@gmail.com>
2024-05-24 13:19:47 +0700

    Remove constant spam

--- Task_2_1_2/src/main/java/ru/nsu/kuklin/parallelprime/Main.java
+++ Task_2_1_2/src/main/java/ru/nsu/kuklin/parallelprime/Main.java
@@ -175,7 +175,6 @@ public class Main {
             return;
         }
         while (true) {
-            System.out.println(newUsers.isEmpty());
             while (!newUsers.isEmpty()) {
                 InetAddress user;
                 try {
