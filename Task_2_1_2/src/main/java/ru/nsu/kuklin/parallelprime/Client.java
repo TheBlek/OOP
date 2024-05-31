7b8e58d18e91d52feb3d0713815673029dd82190
Georgiy Kuklin <egorka99966@gmail.com>
2024-05-31 08:39:06 +0700

    Change port for healthcheck thread

--- Task_2_1_2/src/main/java/ru/nsu/kuklin/parallelprime/Client.java
+++ Task_2_1_2/src/main/java/ru/nsu/kuklin/parallelprime/Client.java
@@ -149,7 +149,7 @@ public class Client {
 
         // Healthcheck thread
         new Thread(() -> {
-            var broadcast = getBroadcast(port);
+            var broadcast = getBroadcast(port - 1);
             if (broadcast == null) {
                 return;
             }
